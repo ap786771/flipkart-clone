c585c044656801af9b21f816e7e4be749d79e058
ap786771 <154984891+ap786771@users.noreply.github.com>
2025-07-06 21:20:04 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProductList from './components/ProductList';
+function App() {
+  return (
+    <div>
+      <h1>Flipkart Clone</h1>
+      <ProductList />
+    </div>
+  );
+}
+export default App;
